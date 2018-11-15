d3a3b0fde2e78b1e14ae2db6cb098d2aa5729716
larda1833031 <larda1833031@etu.cegepjonquiere.ca>
2018-11-15 10:47:55 -0500

    Joueur Fini

---
+++ Exercices JS/Exercice _ Joueurs/JS/traitement.js
@@ -0,0 +1,158 @@
+var Nom,Points;
+var cptNom=0;nombrePoint=0;
+var tabPoint = new Array(4);
+var tabJoueur = new Array(4);
+
+function btnRechercher_onclick()
+{
+    rechercherJoueur();
+}
+
+function  btnAjouter_onclick()
+{
+    saisirInformation();
+    calculerJoueurs();
+    ajouterCondition();
+    changerJoueur();
+}
+
+function btnTrouverMoy_onclick()
+{
+    var moyenne;
+    moyenne = saisirMoyenne();
+    document.getElementById("lblReponse").innerHTML="Le meilleur pointage est de :" + tabPoint[retour] +" réalisé par " + tabJoueur[retour];
+    return moyenne;
+}
+
+function btnTrouverMeilleur_onclick()
+{
+    var retour;
+    retour = SaisirMeilleurPointage();
+    document.getElementById("lblReponse").innerHTML="Le meilleur pointage est de :" + tabPoint[retour] +" réalisé par " + tabJoueur[retour];
+    return retour;
+
+}
+
+function btnTrouverPire_onclick()
+{
+    var retour;
+
+    retour = SaisirPirePointage();
+    document.getElementById("lblReponse").innerHTML="Le pire pointage est de :" + tabPoint[retour] +" réalisé par " + tabJoueur[retour];
+    return retour;
+}
+
+function saisirInformation()
+{
+    Nom=document.getElementById("txtNom").value;
+    Points= parseInt(document.getElementById("txtPoints").value);
+}
+
+function calculerJoueurs()
+{
+    cptNom++;
+    tabJoueur[cptNom-1]=Nom;
+    tabPoint[cptNom-1]=Points;
+}
+
+function saisirMoyenne ()
+{
+    var i,moyenne=0;
+    for(i=0;i<tabPoint.length;i++)
+    {
+        moyenne+=tabPoint[i];
+    }
+    moyenne=moyenne/tabPoint.length;
+    document.getElementById("lblReponse").innerHTML="La moyenne est de  " + moyenne;
+
+    return moyenne;
+}
+
+function SaisirMeilleurPointage()
+{
+    var i,meilleurJoueur,meilleurPointage;
+
+    var max=0;
+
+    for(i=0;i<tabPoint.length;i++)
+    {
+        if(max<=tabPoint[i])
+        {
+            max=tabPoint[i];
+            meilleurPointage=tabPoint[i];
+            meilleurJoueur=tabJoueur[i];
+
+        }
+    }
+    document.getElementById("lblReponse").innerHTML="Le meilleur pointage est de  " + meilleurPointage + " réalisé par "+meilleurJoueur;
+
+    return meilleurPointage;
+    return meilleurJoueur;
+}
+
+function ajouterCondition() {
+
+    if ( cptNom>=0&&cptNom>3)
+    {
+        document.getElementById("btnAjouter").disabled = true;
+        document.getElementById("btnRechercher").disabled = false;
+        document.getElementById("btnTrouverMoy").disabled = false;
+        document.getElementById("btnTrouverMeilleur").disabled = false;
+        document.getElementById("btnTrouverPire").disabled = false;
+    }
+}
+
+function SaisirPirePointage()
+{
+    var i,pireJoueur,pirePoints;
+
+    var pire=100;
+
+    for(i=0;i<tabPoint.length;i++)
+    {
+        if(pire>=tabPoint[i])
+        {
+            pire=tabPoint[i];
+            pirePoints=tabPoint[i];
+            pireJoueur=tabJoueur[i];
+            retour = i;
+        }
+
+    }
+    document.getElementById("lblReponse").innerHTML="Le pire pointage est de  " +pirePoints + " réalisé par "+ pireJoueur;
+
+    return pireJoueur;
+    return pirePoints;
+}
+
+function changerJoueur()
+{
+    document.getElementById("lblNbreJoueur").innerHTML="Numéro du joueur " +(cptNom+1);
+}
+
+function rechercherJoueur()
+{
+    var trouver=false;
+    var Nom=document.getElementById("txtNom").value;
+    cptNom=0;
+    while(trouver==false && cptNom<=3)
+    {
+        if (tabJoueur[cptNom]==Nom)
+        {
+            trouver=true;
+        }
+        else
+        {
+            cptNom++;
+        }
+    }
+    if(trouver==true)
+    {
+        document.getElementById("lblReponse").innerHTML=tabJoueur[cptNom]+" a obtenu un pointage de "+tabPoint[cptNom]+" points ";
+    }
+    else
+    {
+        document.getElementById("lblReponse").innerHTML="Aucun nom ne correspond ";
+    }
+}
+
